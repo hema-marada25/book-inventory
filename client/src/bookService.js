73a36e58cdb360a9830bbc6e2b20cad888188fd8
hema-marada25 <hema.marada22@gmail.com>
2025-10-26 16:01:58 +0530

    url fixes

--- client/src/bookService.js
+++ client/src/bookService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://book-inventory-5qa9.onrender.com/api/';
+const API_BASE_URL = 'https://book-inventory-5qa9.onrender.com/api';
 
 export const getBooks = () => axios.get(`${API_BASE_URL}/books`);
 export const addBook = (book) => axios.post(`${API_BASE_URL}/book`, book);
